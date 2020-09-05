b73d418fcfaa10e7f62c2a11e00abf4e6959c442
damia <damia@DAMIANS-SURFACE>
2020-09-05 13:11:24 -0500

    bb

--- CSharp.Assignments.Selection1/Logic1.cs
+++ CSharp.Assignments.Selection1/Logic1.cs
@@ -7,14 +7,26 @@ namespace CSharp.Assignments.Loop1
     public class Logic1
     {
         /// <summary>
-        /// When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return true if the party with the given values is successful, or false otherwise.
+        /// When squirrels get together for a party, they like to have cigars. 
+        /// A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. 
+        /// Unless it is the weekend, in which case there is no upper bound on the number of cigars. 
+        /// Return true if the party with the given values is successful, or false otherwise.
         /// </summary>
         /// <param name="cigars"></param>
         /// <param name="isWeekend"></param>
         /// <returns></returns>
         public static bool CigarParty(int cigars, bool isWeekend)
         {
-            throw new NotImplementedException();
+            if (isWeekend && cigars >= 40)
+            {
+               return true;
+            }
+            else if (cigars >= 40 && cigars <= 60)
+            {
+                return true;
+            }
+            return false;
+
         }
 
         /// <summary>
